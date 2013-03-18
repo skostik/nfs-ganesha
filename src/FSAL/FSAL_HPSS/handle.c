2da74450e318f0e4042811e13e4163dc344fad14
Dominique Martinet <dominique.martinet@cea.fr>
2013-03-18 14:16:48 +0100

    FSAL_HPSS: Prefix hpss function names with hpss_

--- src/FSAL/FSAL_HPSS/handle.c
+++ src/FSAL/FSAL_HPSS/handle.c
@@ -50,12 +50,12 @@
 /* helpers
  */
 
-/* alloc_handle
+/* hpss_alloc_handle
  * allocate and fill in a handle
  * this uses malloc/free for the time being.
  */
 
-static struct hpss_fsal_obj_handle *alloc_handle(struct hpss_file_handle *fh,
+static struct hpss_fsal_obj_handle *hpss_alloc_handle(struct hpss_file_handle *fh,
                                                 struct attrlist *attr,
                                                 char *link_content,
                                                 struct fsal_export *exp_hdl)
@@ -213,7 +213,7 @@ static fsal_status_t hpss_lookup(struct fsal_obj_handle *parent,      /* IN */
     return status;
 
   /* set output handle */
-  hdl = alloc_handle(NULL, &fsal_attr, NULL, parent_obj_handle->obj_handle.export);
+  hdl = hpss_alloc_handle(NULL, &fsal_attr, NULL, parent_obj_handle->obj_handle.export);
 
   if (hdl == NULL)
     return fsalstat(ERR_FSAL_NOMEM, 0);
@@ -261,7 +261,7 @@ fsal_status_t hpss_lookup_path(struct fsal_export *exp_hdl,
     return status;
 
   /* set output handle */
-  hdl = alloc_handle(NULL, &fsal_attr, NULL, exp_hdl);
+  hdl = hpss_alloc_handle(NULL, &fsal_attr, NULL, exp_hdl);
 
   if (hdl == NULL)
     return fsalstat(ERR_FSAL_NOMEM, 0);
@@ -412,7 +412,7 @@ static fsal_status_t hpss_create( struct fsal_obj_handle *dir_hdl,
 
 
   /* set output handle */
-  hdl = alloc_handle(NULL, attrib, NULL, dir_hdl->export);
+  hdl = hpss_alloc_handle(NULL, attrib, NULL, dir_hdl->export);
 
   if (hdl == NULL)
     return fsalstat(ERR_FSAL_NOMEM, 0);
@@ -531,7 +531,7 @@ static fsal_status_t hpss_mkdir( struct fsal_obj_handle *dir_hdl,
 
 
   /* set output handle   */
-  hdl = alloc_handle(NULL, attrib, NULL, dir_hdl->export);
+  hdl = hpss_alloc_handle(NULL, attrib, NULL, dir_hdl->export);
 
   if (hdl == NULL)
     return fsalstat(ERR_FSAL_NOMEM, 0);
@@ -665,7 +665,7 @@ static fsal_status_t hpss_makesymlink(struct fsal_obj_handle *dir_hdl,
 
 
   /* set output handle */
-  hdl = alloc_handle(NULL, attrib, NULL, dir_hdl->export);
+  hdl = hpss_alloc_handle(NULL, attrib, NULL, dir_hdl->export);
 
   if (hdl == NULL)       
     return fsalstat(ERR_FSAL_NOMEM, 0);
@@ -1752,7 +1752,7 @@ fsal_status_t hpss_create_handle( struct fsal_export *exp_hdl,
     }
 
   /* set output handle */
-  hdl = alloc_handle(&fh, &fsal_attr, link_content, exp_hdl);
+  hdl = hpss_alloc_handle(&fh, &fsal_attr, link_content, exp_hdl);
 
   if (hdl == NULL)
     return fsalstat(ERR_FSAL_NOMEM, 0);
