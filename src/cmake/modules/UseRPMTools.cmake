890f7342ca03d476e1a1db814f2096b76fbe5a2e
Philippe DENIEL <philippe.deniel@cea.fr>
2013-03-18 10:38:15 +0100

    Fixed broken RPM generation. Generated srpm is still faulty.

--- src/cmake/modules/UseRPMTools.cmake
+++ src/cmake/modules/UseRPMTools.cmake
@@ -213,7 +213,15 @@ mkdir -p $RPM_BUILD_ROOT%{_sysconfdir}/init.d
 mkdir -p $RPM_BUILD_ROOT%{_sysconfdir}/sysconfig
 mkdir -p $RPM_BUILD_ROOT%{_sysconfdir}/logrotate.d
 mkdir -p $RPM_BUILD_ROOT%{_bindir}
-mkdir -p $RPM_BUILD_ROOT%{_libdir}
+mkdir -p $RPM_BUILD_ROOT%{_libdir}/ganesha
+
+install -m 644 config_samples/logrotate_ganesha   $RPM_BUILD_ROOT%{_sysconfdir}/logrotate.d/ganesha
+install -m 755 ganesha.init                       $RPM_BUILD_ROOT%{_sysconfdir}/init.d/ganesha
+install -m 755 ganesha.sysconfig                  $RPM_BUILD_ROOT%{_sysconfdir}/sysconfig/ganesha
+
+install -m 644 config_samples/ganesha.conf   $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
+install -m 644 config_samples/hosts.ganesha  $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
+install -m 644 config_samples/snmp.conf      $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
 
 cd ../build_tree
 make install
@@ -234,7 +242,7 @@ if(USE_FSAL_CEPH)
 "
 %files ceph
 %defattr(-,root,root,-)
-%{_libdir}/libfsalceph*
+%{_libdir}/ganesha/libfsalceph*
 
 " )
 endif(USE_FSAL_CEPH)
@@ -244,7 +252,7 @@ if(USE_FSAL_LUSTRE)
 "
 %files lustre
 %defattr(-,root,root,-)
-%{_libdir}/libfsallustre*
+%{_libdir}/ganesha/libfsallustre*
 
 " )
 endif(USE_FSAL_LUSTRE)
@@ -254,7 +262,7 @@ if(USE_FSAL_POSIX)
 "
 %files posix
 %defattr(-,root,root,-)
-%{_prefix}/lib/libfsalposix*
+%{_libdir}/ganesha/libfsalposix*
 
 " )
 endif(USE_FSAL_POSIX)
@@ -264,7 +272,7 @@ if(USE_FSAL_SHOOK)
 "
 %files shook
 %defattr(-,root,root,-)
-%{_libdir}/libfsalshook*
+%{_libdir}/ganesha/libfsalshook*
 
 " )
 endif(USE_FSAL_SHOOK)
@@ -275,7 +283,7 @@ if(USE_FSAL_VFS)
 "
 %files vfs
 %defattr(-,root,root,-)
-%{_prefix}/lib/libfsalvfs*
+%{_libdir}/ganesha/libfsalvfs*
 
 " )
 endif(USE_FSAL_VFS)
@@ -285,7 +293,7 @@ if(USE_FSAL_HPSS)
 "
 %files hpss
 %defattr(-,root,root,-)
-%{_prefix}/lib/libfsalhpss*
+%{_libdir}/ganesha/libfsalhpss*
 
 " )
 endif(USE_FSAL_HPSS)
@@ -295,7 +303,7 @@ if(USE_FSAL_PROXY)
 "
 %files proxy
 %defattr(-,root,root,-)
-%{_prefix}/lib/libfsalproxy*
+%{_libdir}/ganesha/libfsalproxy*
 
 " )
 endif(USE_FSAL_PROXY)
@@ -305,7 +313,7 @@ if(USE_FSAL_ZFS)
 "
 %files zfs
 %defattr(-,root,root,-)
-%{_prefix}/lib/libfsalzfs*
+%{_libdir}/ganesha/libfsalzfs*
 
 " )
 endif(USE_FSAL_ZFS)
@@ -321,13 +329,13 @@ FILE(APPEND ${RPM_ROOTDIR}/SPECS/${RPMNAME}.spec ${RPM_CHANGELOG_FILE_CONTENT} )
       ADD_CUSTOM_TARGET(${RPMNAME}_srpm
 	COMMAND cpack -G TGZ --config CPackSourceConfig.cmake
 	COMMAND ${CMAKE_COMMAND} -E copy ${CPACK_SOURCE_PACKAGE_FILE_NAME}.tar.gz ${RPM_ROOTDIR}/SOURCES    
-	COMMAND ${RPMTools_RPMBUILD_EXECUTABLE} --verbose -bs --define=\"_topdir ${RPM_ROOTDIR}\" ${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME} 
+	COMMAND ${RPMTools_RPMBUILD_EXECUTABLE} -bs --define=\"_topdir ${RPM_ROOTDIR}\" ${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME} 
 	)
       
       ADD_CUSTOM_TARGET(${RPMNAME}_rpm
 	COMMAND cpack -G TGZ --config CPackSourceConfig.cmake
 	COMMAND ${CMAKE_COMMAND} -E copy ${CPACK_SOURCE_PACKAGE_FILE_NAME}.tar.gz ${RPM_ROOTDIR}/SOURCES    
-	COMMAND ${RPMTools_RPMBUILD_EXECUTABLE} --verbose -bb --define=\"_topdir ${RPM_ROOTDIR}\" ${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME} 
+	COMMAND ${RPMTools_RPMBUILD_EXECUTABLE} -bb --define=\"_topdir ${RPM_ROOTDIR}\" ${RPM_ROOTDIR}/SPECS/${SPECFILE_NAME} 
 	)  
     ENDMACRO(RPMTools_ADD_RPM_TARGETS)
 
