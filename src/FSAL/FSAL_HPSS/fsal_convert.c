43f26f4843e0d7fde17412ccd243c83974731c62
Dominique Martinet <dominique.martinet@cea.fr>
2013-03-18 14:17:52 +0100

    FSAL_HPSS: Allow (and ignore) setattr on SPACEUSED attribute, because nfs3 sets it along with ATTR_SIZE

--- src/FSAL/FSAL_HPSS/fsal_convert.c
+++ src/FSAL/FSAL_HPSS/fsal_convert.c
@@ -786,7 +786,7 @@ fsal_status_t fsal2hpss_attribset(struct fsal_obj_handle * p_fsal_handle,
 
   /* Settable attrs. */
 
-  settable_attrs = (ATTR_SIZE | ATTR_ACL |
+  settable_attrs = (ATTR_SIZE | ATTR_SPACEUSED | ATTR_ACL |
                     ATTR_MODE | ATTR_OWNER |
                     ATTR_GROUP | ATTR_ATIME |
                     ATTR_CTIME | ATTR_MTIME |
